00a889545a3eca07a1ec34476b6c0bc317d5190b
unknown <katee98@yandex.ru>
2019-01-22 22:45:52 +0300

    homework-2

---
+++ main.js
@@ -0,0 +1,12 @@
+let age = 10;
+if(age >= 1 && age <= 17) {
+    console.log("Учеба");
+}
+
+if(age >= 18 && age <= 64) {
+    console.log("Работа");
+}
+
+if(age >= 65 && age <= 100) {
+    console.log("Пенсия");
+}
